4553b9d70889b3867c0dd8a5ac08e5cd5553915c
Samuel <samuelschutz14@gmail.com>
2025-08-19 20:30:27 -0300

    add: aula4 code

---
+++ aula4.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    float altura;
+    float profundidade;
+    float largura;
+} REGISTRO;
+
+void createRegister(REGISTRO reg[], int length) {
+    reg = malloc((length + 1) * sizeof(REGISTRO));
+
+    if(!reg) {
+        printf("Erro alocar memoria");
+        return;
+    }
+
+    for(int i = 0; i < length; i++) {
+        printf("Altura: ");
+        scanf("%f", &reg[i].altura);
+        printf("Profundidade: ");
+        scanf("%f", &reg[i].profundidade);
+        printf("Largura: ");
+        scanf("%f", &reg[i].largura);
+    }
+}
+
+void editRegister(REGISTRO reg[], int *length) {
+    int i;
+    printf("Indice do registro a ser editado: ");
+    scanf("%d", &i);
+
+    if(i < 1 || i > *length) {
+        printf("Indice invalido\n");
+        return;
+    }
+
+    printf("Entre com os novos valores para o registro %d\n", i);
+    printf("Altura: ");
+    scanf("%f", &reg[i-1].altura);
+    printf("Profundidade: ");
+    scanf("%f", &reg[i-1].profundidade);
+    printf("Largura: ");
+    scanf("%f", &reg[i-1].largura);
+}
+
+void deleteRegister(REGISTRO reg[], int *length) {
+    int i;
+    printf("Indice do registro a ser excluido: ");
+    scanf("%d", &i);
+
+    if(i < 1 || i > *length) {
+        printf("Indice invalido\n");
+        return;
+    }
+
+    for(int j = i-1; j < *length-1; j++) {
+        reg[j] = reg[j+1];
+    }
+
+    (*length)--;
+}
+
+void listRegisters(REGISTRO reg[], int *length) {
+    for(int i = 0; i < *length; i++) {
+        printf("Registro %d: Altura = %.2f, Profundidade = %.2f, Largura = %.2f\n", i+1, reg[i].altura, reg[i].profundidade, reg[i].largura);
+    }
+
+}
+
+int main() {
+    REGISTRO *reg;
+    int length, opt;
+
+    while(opt != 5) {
+        printf("-------------------------------------------\n");
+        printf("1 - Inserir registro\n");
+        printf("2 - Editar registro\n");
+        printf("3 - Excluir registro\n");
+        printf("4 - Listar registros\n");
+        printf("5 - Sair\n");
+        printf("-------------------------------------------\n");
+        printf("Informe um numero para a opcao: ");
+        scanf("%d", &opt);
+        printf("-------------------------------------------\n");
+
+        switch(opt) {
+            case 1:
+                createRegister(reg, &length);
+                break;
+            case 2:
+                editRegister(reg, &length);
+                break;
+            case 3:
+                deleteRegister(reg, &length);
+                break;
+            case 4:
+                listRegisters(reg, &length);
+                break;
+            case 5:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida\n");
+                break;
+        }
+    };
+
+    free(reg);
+    return 0;
+}
